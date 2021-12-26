ecd8c88db63f1c541e6c9fce7b1a357126d05e3e
enzo <enzo@simbo.com.tw>
2021-12-26 14:37:17 +0800

    add chatMessage

--- src/main/java/com/example/chatroom/model/ChatMessage.java
+++ src/main/java/com/example/chatroom/model/ChatMessage.java
@@ -1,4 +1,11 @@
 package com.example.chatroom.model;
 
+import com.example.chatroom.codes.ChatType;
+import lombok.Data;
+
+@Data
 public class ChatMessage {
+    private ChatType chatType;
+    private String message;
+    private String content;
 }
