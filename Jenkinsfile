dda306f1b33d10f1af60956b6175aac30b870032
enzo <enzo@simbo.com.tw>
2022-01-27 22:33:28 +0800

    add jenkins

---
+++ Jenkinsfile
@@ -0,0 +1,14 @@
+pipeline {
+    agent any
+    tools {
+        maven 'Maven 3.8.1'
+        jdk 'JDK 1.8'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/chatroom/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.chatroom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World Spring Boot build by Jenkins Maven plugin";
+    }
+}
